61dc2fd33e42d5dade86d64dbff4ab84bab4c043
riswanmsm <msmr1986@gmail.com>
2023-03-12 18:08:54 +0530

    python unit test practice

--- practice_labs/FirstWebApplication.py
+++ practice_labs/FirstWebApplication.py
@@ -8,6 +8,6 @@ def hello():
     return "Hello World!"
 
 
-if __name__=="__main__":
-    app.run(debug=True) 
+if __name__ == "__main__":
+    app.run(debug=True)
     # When no port is specified, starts at default port 5000
